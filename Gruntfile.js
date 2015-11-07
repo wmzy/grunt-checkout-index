d2354a3a916a526ced995d34cdb1acd00cb8729f
wmzy <1256573276@qq.com>
2015-11-07 18:31:00 +0800

    code format

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*
- * grunt-copy-git-index
+ * Grunt-copy-git-index
  *
  * Copyright (c) 2015 wmzy
  * Licensed under the MIT license.
@@ -8,59 +8,58 @@
 'use strict';
 
 module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: {
+        src: [
+          'Gruntfile.js',
+          'tasks/**/*.js',
+          'test/**/*.js'
+        ],
+        options: {
+          jshintrc: true
+        }
+      }
+    },
+    jscs: {
+      src: [
+        'Gruntfile.js',
+        'tasks/**/*.js',
+        'test/**/*.js'
+      ],
+      options: {
+        config: '.jscsrc',
+        verbose: true // If you need output with rule names http://jscs.info/overview.html#verbose
+      }
+    },
+    mochaTest: {
+      src: ['test/*.js'],
+      options: {
+        reporter: 'spec',
+        require: [
+          function () {
+            var should = require('should');
+          }
+        ]
+      }
+    }
+  });
 
-	// Project configuration.
-	grunt.initConfig({
-		jshint: {
-			all: {
-				src: [
-					'Gruntfile.js',
-					'tasks/**/*.js',
-					'test/**/*.js'
-				],
-				options: {
-					jshintrc: true
-				}
-			}
-		},
-		jscs: {
-			src: [
-				'Gruntfile.js',
-				'tasks/**/*.js',
-				'test/**/*.js'
-			],
-			options: {
-				config: '.jscsrc',
-				verbose: true // If you need output with rule names http://jscs.info/overview.html#verbose
-			}
-		},
-		mochaTest: {
-			src: ['test/*.js'],
-			options: {
-				reporter: 'spec',
-				require: [
-					function () {
-						var should = require('should');
-					}
-				]
-			}
-		}
-	});
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
+  grunt.registerTask('build', 'Build plugin.', function () {
+    // Todo: build
+  });
 
-	grunt.registerTask('build', 'Build plugin.', function () {
-		// todo: build
-	});
+  // Load NPM tasks
+  require('load-grunt-tasks')(grunt);
 
-	// Load NPM tasks
-	require('load-grunt-tasks')(grunt);
+  // Whenever the "test" task is run, run the "unit-test" task.
+  grunt.registerTask('test', ['jshint', 'jscs', 'mochaTest']);
 
-	// Whenever the "test" task is run, run the "unit-test" task.
-	grunt.registerTask('test', ['jshint', 'jscs', 'mochaTest']);
-
-	// By default, lint and run all tests.
-	grunt.registerTask('default', ['test', 'build']);
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['test', 'build']);
 
 };
